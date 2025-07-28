de9fe8d4da6baa6cf53cebf1932d5a39949eea25
fatimayyl <yaylifatima@gmail.com>
2025-07-28 03:26:59 +0300

    Update response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -2,9 +2,6 @@
 from sdks.novavision.src.helper.package import PackageHelper
 from capsules.FacialEmotionRecognition.src.models.PackageModel import FacialEmotionRecognitionExecutor, PackageModel, PackageConfigs, FacialEmotionRecognitionResponse, FacialEmotionRecognitionOutputs, OutputImage, ConfigExecutor, OutputDetections
 
-from capsules.FacialEmotionRecognition.src.models.PackageModel import FacialEmotionRecognitionDeepFaceExecutor,FacialEmotionRecognitionDeepFaceResponse, FacialEmotionRecognitionDeepFaceOutputs
-
-
 def build_response(context):
     emotionOutput = OutputImage(value=context.image)
     emotionDetection = OutputDetections(value=context.prediction)
@@ -16,20 +13,5 @@ def build_response(context):
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
     return packageModel
-
-def build_response_deepface(context):
-    output_image = OutputImage(value=context.image)
-    output_detections = OutputDetections(value=context.image.detections)
-    outputs = FacialEmotionRecognitionDeepFaceOutputs(outputImage=output_image,outputDetections=output_detections)
-    response = FacialEmotionRecognitionDeepFaceResponse(outputs=outputs)
-    deepface_executor = FacialEmotionRecognitionDeepFaceExecutor(value=response)
-    executor = ConfigExecutor(value=deepface_executor)
-    package_configs = PackageConfigs(executor=executor)
-    package = PackageHelper(packageModel=PackageModel, packageConfigs=package_configs)
-    package_model = package.build_model(context)
-    return package_model
-
-
-
 
 
