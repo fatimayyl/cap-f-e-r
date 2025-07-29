c8be7c1f79046074f563edf5c75cf4eb53ec9137
fatimayyl <yaylifatima@gmail.com>
2025-07-29 16:32:06 +0300

    Update response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,15 +1,32 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
 from capsules.FacialEmotionRecognition.src.models.PackageModel import FacialEmotionRecognitionExecutor, PackageModel, PackageConfigs, FacialEmotionRecognitionResponse, FacialEmotionRecognitionOutputs, OutputImage, ConfigExecutor, OutputDetections
+from capsules.FacialEmotionRecognition.src.models.PackageModel import FacialEmotionRecognitionDeepFaceExecutor, FacialEmotionRecognitionDeepFaceOutputs, FacialEmotionRecognitionDeepFaceResponse
+
 
 def build_response(context):
-    emotionOutput = OutputImage(value=context.image)
-    emotionDetection = OutputDetections(value=context.prediction)
-    emotionOutputs = FacialEmotionRecognitionOutputs(outputDetections=emotionDetection, outputImage=emotionOutput)
-    emotionResponse = FacialEmotionRecognitionResponse(outputs=emotionOutputs)
-    emotionExecutor = FacialEmotionRecognitionExecutor(value=emotionResponse)
-    executor = ConfigExecutor(value=emotionExecutor)
+    outputImage = OutputImage(value=context.outputImage)
+    outputDetections = OutputDetections(value=context.outputDetections)
+    facialEmotionRecognitionOutputs = FacialEmotionRecognitionOutputs(outputDetections=outputDetections, outputImage=outputImage)
+    facialEmotionRecognitionResponse = FacialEmotionRecognitionResponse(outputs=facialEmotionRecognitionOutputs)
+    facialEmotionRecognitionExecutor = FacialEmotionRecognitionExecutor(value=facialEmotionRecognitionResponse)
+    executor = ConfigExecutor(value=facialEmotionRecognitionExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
     return packageModel
+
+
+
+def build_response_deepface(context):
+    outputImage = OutputImage(value=context.outputImage)
+    outputDetections = OutputDetections(value=context.outputDetections)
+    facialEmotionRecognitionDeepFaceOutputs = FacialEmotionRecognitionDeepFaceOutputs(outputDetections=outputDetections, outputImage=outputImage)
+    facialEmotionRecognitionDeepFaceResponse = FacialEmotionRecognitionDeepFaceResponse(outputs=facialEmotionRecognitionDeepFaceOutputs)
+    facialEmotionRecognitionDeepFaceExecutor = FacialEmotionRecognitionDeepFaceExecutor(value=facialEmotionRecognitionDeepFaceResponse)
+    executor = ConfigExecutor(value=facialEmotionRecognitionDeepFaceExecutor)
+    packageConfigs = PackageConfigs(executor=executor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
