0630920d460c286e0bdaac340890088829148168
fatimayyl <yaylifatima@gmail.com>
2025-08-04 15:38:18 +0300

    Update response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,15 +1,33 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from capsules.FacialEmotionRecognition.src.models.PackageModel import FacialEmotionRecognitionExecutor, PackageModel, PackageConfigs, FacialEmotionRecognitionResponse, FacialEmotionRecognitionOutputs, OutputImage, ConfigExecutor, OutputDetections
+from capsules.FacialEmotionRecognition.src.models.PackageModel import FacialEmotionRecognitionExecutor, PackageModel, PackageConfigs, FacialEmotionRecognitionResponse, FacialEmotionRecognitionOutputs, OutputDetections, ConfigExecutor, OutputDetections
 from capsules.FacialEmotionRecognition.src.models.PackageModel import FacialEmotionRecognitionDeepFaceExecutor, FacialEmotionRecognitionDeepFaceOutputs, FacialEmotionRecognitionDeepFaceResponse
+
+
+"""
+def build_response(context):
+    print("DEBUG: context.prediction =", context.prediction)
+
+    outputImage = OutputDetections(value=context.image)
+    outputDetections = OutputDetections(value=context.prediction)
+    facialEmotionRecognitionOutputs = FacialEmotionRecognitionOutputs(outputDetections=outputDetections, outputImage=outputImage)
+    facialEmotionRecognitionResponse = FacialEmotionRecognitionResponse(outputs=facialEmotionRecognitionOutputs)
+    facialEmotionRecognitionExecutor = FacialEmotionRecognitionExecutor(value=facialEmotionRecognitionResponse)
+    executor = ConfigExecutor(value=facialEmotionRecognitionExecutor)
+    packageConfigs = PackageConfigs(executor=executor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+"""
 
 
 def build_response(context):
     print("DEBUG: context.prediction =", context.prediction)
 
-    outputImage = OutputImage(value=context.image)
+    # OutputImage istiyorsan önce tanımla yoksa sil
+    # Eğer sadece detection dönüyorsan outputImage'ı kaldır
     outputDetections = OutputDetections(value=context.prediction)
-    facialEmotionRecognitionOutputs = FacialEmotionRecognitionOutputs(outputDetections=outputDetections, outputImage=outputImage)
+    facialEmotionRecognitionOutputs = FacialEmotionRecognitionOutputs(outputDetections=outputDetections)
     facialEmotionRecognitionResponse = FacialEmotionRecognitionResponse(outputs=facialEmotionRecognitionOutputs)
     facialEmotionRecognitionExecutor = FacialEmotionRecognitionExecutor(value=facialEmotionRecognitionResponse)
     executor = ConfigExecutor(value=facialEmotionRecognitionExecutor)
@@ -21,7 +39,7 @@ def build_response(context):
 
 
 def build_response_deepface(context):
-    outputImage = OutputImage(value=context.image)
+    outputImage = OutputDetections(value=context.image)
     outputDetections = OutputDetections(value=context.prediction)
     facialEmotionRecognitionDeepFaceOutputs = FacialEmotionRecognitionDeepFaceOutputs(outputDetections=outputDetections, outputImage=outputImage)
     facialEmotionRecognitionDeepFaceResponse = FacialEmotionRecognitionDeepFaceResponse(outputs=facialEmotionRecognitionDeepFaceOutputs)
